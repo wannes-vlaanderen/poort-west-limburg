9fa0b4dc9fe52ce5642b513a98e3d623e3b1d841
wannes-vlaanderen <138599016+wannes-vlaanderen@users.noreply.github.com>
2023-07-17 15:29:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -276,53 +276,6 @@ map.addControl(new mapboxgl.AttributionControl({
   customAttribution: '<a href="https://omgeving.vlaanderen.be/nl/ena-economisch-netwerk-albertkanaal">ENA</a> <a href="https://vlaanderen.be">Vlaamse Overheid</a>'
 }));
 
-class LogoVlaanderen {
-  onAdd(map) {
-    this._map = map;
-    this._container = document.createElement('img');
-    this._container.className = 'mapboxgl-ctrl';
-    this._container.src = 'https://assets.vlaanderen.be/image/upload/widgets/vlaanderen-is-omgeving-logo.svg';
-    this._container.width = 120
-    this._container.href = "https://omgeving.vlaanderen.be"
-    return this._container;
-  }
-
-  onRemove() {
-    this._container.parentNode.removeChild(this._container);
-    this._map = undefined;
-  }
-}
 map.addControl(new LogoVlaanderen(), "top-left")
 
-class Legenda {
-  onAdd(map) {
-    this._map = map;
-    this._container = document.createElement('div');
-    this._container.className = 'mapboxgl-ctrl';
-    this._container.appendChild(this.newEntry("nieuw_bedrijventerrein", "Nieuw Bedrijventerrein"))
-    this._container.appendChild(this.newEntry("bestaand_bedrijventerrein", "Bestaand Bedrijventerrein"))
-    this._container.appendChild(this.newEntry("bos", "Natuurgebied"))
-    this._container.appendChild(this.newEntry("regio", "Regio"))
-    this._container.appendChild(this.newEntry("infrastructuurproject", "Infrastructuurproject"))
-    this._container.width = 120
-    this._container.classList.add("legenda")
-    return this._container;
-  }
-  
-  newEntry(color, text) {
-    var container = document.createElement("div")
-    var square = document.createElement("div")
-    square.classList.add("box", color);
-    container.innerHTML = text;
-    
-    // add child
-    container.appendChild(square)
-    return container
-  }
-
-  onRemove() {
-    this._container.parentNode.removeChild(this._container);
-    this._map = undefined;
-  }
-}
 map.addControl(new Legenda(map), "top-right")
